2239fbd8206b10886c7ef63cfb2efd85f779a3d6
Pranay <Pranay@Pranay-Machine.Home>
2014-03-15 10:04:30 +0530

    First Commoi

---
+++ src/FingerName.java
@@ -0,0 +1,33 @@
+
+
+/* Names of the fingers, ordered counter-clockwise for the left hand
+
+*/
+
+
+public enum FingerName {
+   LITTLE, RING, MIDDLE, INDEX, THUMB, 
+UNKNOWN;
+//  THUMB,INDEX,MIDDLE,RING,LITTLE,UNKNOWN;
+
+  public FingerName getNext() 
+  {  
+    int nextIdx = ordinal()+1;
+    if (nextIdx == (values().length))
+      nextIdx = 0;
+    return values()[nextIdx];  
+  }  // end of getNext()
+
+
+  public FingerName getPrev() 
+  {  
+    int prevIdx = ordinal()-1;
+    if (prevIdx < 0)
+      prevIdx = values().length-1;
+    return values()[prevIdx];  
+  }  // end of getPrev()
+
+}  // end of FingerName enum
+
+
+
---
+++ src/Handy.java
@@ -0,0 +1,71 @@
+
+
+/* Detect the user's gloved hand and fingers, drawing information
+   on top of a webcam image.
+
+   Usage:
+   > run Handy
+*/
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+
+import com.googlecode.javacv.*;
+import com.googlecode.javacv.cpp.*;
+import com.googlecode.javacpp.Loader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Handy extends JFrame 
+{
+  // GUI components
+  private HandPanel handPanel;
+
+
+  public Handy()
+  {
+    super("Hand Detector");
+
+    Container c = getContentPane();
+    c.setLayout( new BorderLayout() );   
+
+    // Preload the opencv_objdetect module to work around a known bug.
+    Loader.load(opencv_objdetect.class);
+
+    handPanel = new HandPanel(); // the webcam pictures and drums appear here
+    c.add( handPanel, BorderLayout.CENTER);
+
+    addWindowListener( new WindowAdapter() {
+      public void windowClosing(WindowEvent e)
+      { handPanel.closeDown();    // stop snapping pics, and any drum playing
+        System.exit(0);
+      }
+    });
+
+    setResizable(false);
+    pack();  
+    setLocationRelativeTo(null);
+    setVisible(true);
+  } // end of Handy()
+
+
+  // -------------------------------------------------------
+
+  public static void main( String args[] )
+  {   try {
+          new Handy();
+       
+                     
+          
+          
+           
+      } catch (Exception ex) {
+          Logger.getLogger(Handy.class.getName()).log(Level.SEVERE, null, ex);
+      }
+     
+  }
+
+} // end of Handy class
